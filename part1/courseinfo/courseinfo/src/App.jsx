6c414a12d80ef1215b790ba99e8a899ac1fbc128
William <willpandian@gmail.com>
2025-02-25 18:19:21 +0100

    Refactored code for exercise 1.3

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -8,9 +8,10 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
   return(
     <>
-      <p>{props.part} {props.exercises}</p>
+      <p>{props.part.name} {props.part.exercises}</p>
     </>
   )
 }
@@ -18,9 +19,9 @@ const Part = (props) => {
 const Content = (props) =>{
   return(
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part1}/>
+      <Part part={props.part2}/>
+      <Part part={props.part3}/>
     </>
   )
 }
@@ -52,7 +53,7 @@ const App = () => {
     <div>
       <Header course={course} />
 
-      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
 
       <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </div>
